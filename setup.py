c0aad4d7d9e567cdd9232e667c1aee99802ba762
Jeff Hui <jeff@jeffhui.net>
2012-08-20 23:26:53 -0700

    Added manifest file.

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ with open('README.md') as h:
     README = h.read()
 
 setup(
-    name='python-constraints',
+    name='pyconstraints',
     description='A simple constraints satisfaction solver',
     long_description=README,
     license=LICENSE,
     version='1.0.0',
     author='Jeff Hui',
     author_email='jeff@jeffhui.net',
-    url='http://github.com/jeffh/python-constraints',
+    url='http://github.com/jeffh/pyconstraints',
     packages=['csp'],
     requires=[],
 )
